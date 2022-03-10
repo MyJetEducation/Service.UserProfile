f7ee93d6ceb95dd2149a0c88a45ed072e658615c
dmitriy.b <dmitriy.b@smplt.net>
2022-03-10 15:16:48 +0500

    FIN360PMVP-200: Изменить логику начисления Skills#resolved

--- src/Service.UserProfileApi/Mappers/StatusProgressMapper.cs
+++ src/Service.UserProfileApi/Mappers/StatusProgressMapper.cs
@@ -11,5 +11,16 @@ namespace Service.UserProfileApi.Mappers
 			Index = grpcResponse.Index,
 			Progress = grpcResponse.Progress
 		};
+
+		public static SkillStatusProgressModel ToModel(this SkillProgressGrpcResponse grpcResponse) => new SkillStatusProgressModel
+		{
+			Total = grpcResponse.Total,
+			Activity = grpcResponse.Activity,
+			Adaptability = grpcResponse.Adaptability,
+			Concentration = grpcResponse.Concentration,
+			Memory = grpcResponse.Memory,
+			Perseverance = grpcResponse.Perseverance,
+			Thoughtfulness = grpcResponse.Thoughtfulness
+		};
 	}
 }
--- src/Service.UserProfileApi/Models/ProgressResponse.cs
+++ src/Service.UserProfileApi/Models/ProgressResponse.cs
@@ -20,7 +20,7 @@ namespace Service.UserProfileApi.Models
 
 		public StatusProgressModel Habit { get; set; }
 
-		public StatusProgressModel Skill { get; set; }
+		public SkillStatusProgressModel Skill { get; set; }
 		
 		public StatusProgressModel Knowledge { get; set; }
 
@@ -37,5 +37,29 @@ namespace Service.UserProfileApi.Models
 
 		[Range(1, 100)]
 		public int Progress { get; set; }
+	}
+
+	public class SkillStatusProgressModel
+	{
+		[Range(1, 100)]
+		public int Total { get; set; }
+
+		[Range(1, 100)]
+		public int Concentration { get; set; }
+
+		[Range(1, 100)]
+		public int Perseverance { get; set; }
+
+		[Range(1, 100)]
+		public int Thoughtfulness { get; set; }
+
+		[Range(1, 100)]
+		public int Memory { get; set; }
+
+		[Range(1, 100)]
+		public int Adaptability { get; set; }
+
+		[Range(1, 100)]
+		public int Activity { get; set; }
 	}
 }
